92472d11e279bd1fe222c2527ae6fbb718e6e236
Luís Von Muller <luisvmuller@icloud.com>
2021-11-22 10:14:20 -0300

    Made a Basic template to cp

---
+++ BasicTemplateTypeScript/index.ts
@@ -0,0 +1,30 @@
+/** Set-up terminal env like */
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+/* Global Variables */
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+/** Listen For Data beeing placed on the terminal */
+process.stdin.on('data', function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+/* After adding 2 lines you'd like to type: CTRL+D to Fire "EOF" */
+process.stdin.on('end', function (): void {
+  inputLines = inputString.split('\n');
+  inputString = '';
+
+  main();
+});
+
+/** Read Lines as it goes */
+const readLine = (): string => {
+  return inputLines[currentLine++];
+}
+
+/**------------------------------------------
+ *  TODO: QUESTION CODE GOES BELOW HERE (☞ﾟヮﾟ)☞
+ * ------------------------------------------ */
