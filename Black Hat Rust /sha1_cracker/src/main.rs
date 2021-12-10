1280726e8574f617d9a5ecbdbd63ef3dc321d314
Luís Von Muller <luisvmuller@icloud.com>
2021-12-10 09:36:30 -0300

    LOl

--- Black Hat Rust /sha1_cracker/src/main.rs
+++ Black Hat Rust /sha1_cracker/src/main.rs
@@ -1,8 +1,14 @@
-use std::env;
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 /* Gets buffer like (usize) */
 const SHA1_HEX_STRING_LENGTH: usize = 40;
-fn main() {
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect(); /* Gives brand new Strings */
 
     if args.len() != 3 {
@@ -15,6 +21,11 @@ fn main() {
     if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
         return Err("sha1 hash is not valid".into());
     }
-
-    panic!("I don't have implemented this yet lol")
+    let word_list_file = File::open(&args[1])?;
+    let reader = BufReader::new(&word_list_file);
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        println!("{}", line);
+    }
+    Ok(())
 }
